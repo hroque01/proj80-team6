2abc5263f0ed678509943148a6c7ec4780ef359f
Vale <oirelav95@gmail.com>
2023-02-28 18:14:57 +0100

    add AppFooter in App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,18 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
+import AppFooter from './components/AppFooter.vue';
 
 export default {
   components: {
     AppHeader,
+    AppFooter
   }
 }
 </script>
 
 <template>
   <AppHeader />
+  <AppFooter />
 </template>
 
 <style lang="scss">
