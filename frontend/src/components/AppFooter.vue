a72253c624613e9f48010432a364dd28175d8000
Vale <oirelav95@gmail.com>
2023-02-28 18:15:06 +0100

    create AppFooter.vue

---
+++ frontend/src/components/AppFooter.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    name: 'AppFooter',
+}
+</script>
+
+<template>
+    <footer>Ciao da footer</footer>
+</template>
+
+<style lang="scss" scoped>
+@use '../src/styles/general.scss' as *;
+@use '../src/styles/partials/mixins' as *;
+@use '../src/styles/partials/variables' as *;
+</style>
