d762790aceb776bb3b059fac667084d364f6b4b1
RominaMalimban <romina.lyn@hotmail.it>
2023-03-01 17:35:31 +0100

    finalization section banner

--- frontend/src/components/AppBannerMain.vue
+++ frontend/src/components/AppBannerMain.vue
@@ -7,7 +7,13 @@ export default {
 
 <template>
     <section>
-        <img src="public/img/banner-red.jpeg" alt="">
+        <img src="public/img/banner.jpeg" alt="">
+        <div class="ms_txt">
+            <h1>Up to 25% off - Meal Deals</h1>
+            <p>
+                Need a midweek pick-me-up, a break from cooking for the family or just fancy your favourite restaurant?
+            </p>
+        </div>
     </section>
 </template>
 
@@ -16,5 +22,22 @@ export default {
 @use '../src/styles/partials/mixins' as *;
 @use '../src/styles/partials/variables' as *;
 
+section{
+    position: relative;
 
+    .ms_txt{
+        position: absolute;
+        text-align: center;
+        color: #fff;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+
+        h1{
+            font-size: 50px;
+            margin-bottom: 20px;
+            font-weight: bolder;
+        }
+    }
+}
 </style>
