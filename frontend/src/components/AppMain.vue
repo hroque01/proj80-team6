2d4437c94c1279dee79d12264479e59d2c5cbd2c
Hadson Roque <hadson.roque01@git config --global user.email hadson.roque01@gmail.com>
2023-03-01 17:53:14 +0100

    App main vue conflict

--- frontend/src/components/AppMain.vue
+++ frontend/src/components/AppMain.vue
@@ -1,39 +1,25 @@
 <script>
 
 export default {
-<<<<<<< HEAD
-  name: 'AppMain',
-=======
       name: 'AppMain',
->>>>>>> NavbarNew
 }
 
 </script>
 
 <template>
-<<<<<<< HEAD
-    <!-- main section  -->
-    <main>
-        Ciao da main
-    </main>
-=======
       <!-- main section  -->
       <main>
             Ciao da main
       </main>
->>>>>>> NavbarNew
 </template>
 
 <style lang="scss" scoped>
 @use '../src/styles/general.scss' as *;
 @use '../src/styles/partials/mixins' as *;
 @use '../src/styles/partials/variables' as *;
-<<<<<<< HEAD
-=======
 
 main {
       margin-top: 90px;
 
 }
->>>>>>> NavbarNew
 </style>
