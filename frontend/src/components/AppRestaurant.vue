77e86a183eeae224dc73cbb65d0480b230113a76
RominaMalimban <romina.lyn@hotmail.it>
2023-03-01 19:02:12 +0100

    add flex in AppRestaurant

--- frontend/src/components/AppRestaurant.vue
+++ frontend/src/components/AppRestaurant.vue
@@ -11,12 +11,19 @@ export default {
 </script>
 
 <template>
-  <AppNavRes />
-  <AppMainRes />
+  <main>
+    <AppNavRes />
+    <AppMainRes />
+  </main>
+  
 </template>
 
 <style lang="scss">
 @use '../src/styles/general.scss' as *;
 @use '../src/styles/partials/mixins' as *;
 @use '../src/styles/partials/variables' as *;
+
+main{
+  @include flex(flex);
+}
 </style>
