8b38bc2c23098d7ce6ceecc9cb892a79caff791e
Hadson Roque <hadson.roque01@git config --global user.email hadson.roque01@gmail.com>
2023-03-01 11:29:46 +0100

    Create components AppNavRes.vue

---
+++ frontend/src/components/AppNavRes.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    name: 'AppNavRes',
+}
+</script>
+
+<template>
+    <nav>
+        Ciao da AppNavRes
+    </nav>
+</template>
+
+<style lang="scss" scoped>
+@use '../src/styles/general.scss' as *;
+@use '../src/styles/partials/mixins' as *;
+@use '../src/styles/partials/variables' as *;
+</style>
