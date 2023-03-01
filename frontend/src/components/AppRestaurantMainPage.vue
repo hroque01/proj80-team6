ddd1697b17bdc331d5d02aacd0fa9a7fee07ebb0
Daviduo <davide0400@gmail.com>
2023-03-01 19:49:06 +0100

    added AppRestaurantMainPage component

---
+++ frontend/src/components/AppRestaurantMainPage.vue
@@ -0,0 +1,19 @@
+<script>
+
+export default {
+  name: 'AppRestaurantMainPage',
+  components: {
+    
+  }
+}
+</script>
+
+<template>
+  
+</template>
+
+<style lang="scss">
+@use '../src/styles/general.scss' as *;
+@use '../src/styles/partials/mixins' as *;
+@use '../src/styles/partials/variables' as *;
+</style>
