d8c76e179ddee4075fd987ac7b1696986168f297
RominaMalimban <romina.lyn@hotmail.it>
2023-03-01 17:16:34 +0100

    insert img in AppBannerMain

---
+++ frontend/src/components/AppBannerMain.vue
@@ -0,0 +1,20 @@
+<script>
+
+export default {
+    name: 'AppBannerMain'
+}
+</script>
+
+<template>
+    <section>
+        <img src="public/img/banner-red.jpeg" alt="">
+    </section>
+</template>
+
+<style lang="scss" scoped>
+@use '../src/styles/general.scss' as *;
+@use '../src/styles/partials/mixins' as *;
+@use '../src/styles/partials/variables' as *;
+
+
+</style>
