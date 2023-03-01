28156563f7c76266eb02255d9b1a12e90767ab3e
Hadson Roque <hadson.roque01@git config --global user.email hadson.roque01@gmail.com>
2023-03-01 11:28:39 +0100

    Create /restarant.vue

---
+++ frontend/src/AppRestaurant.vue
@@ -0,0 +1,19 @@
+<script>
+import AppNavRes from './components/AppNavRes.vue';
+
+export default {
+  components: {
+    AppNavRes,
+  }
+}
+</script>
+
+<template>
+  <AppNavRes />
+</template>
+
+<style lang="scss">
+@use '../src/styles/general.scss' as *;
+@use '../src/styles/partials/mixins' as *;
+@use '../src/styles/partials/variables' as *;
+</style>
