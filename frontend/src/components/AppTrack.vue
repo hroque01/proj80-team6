104fa55a226ec9844f43213552196d5a21ad61be
Hadson Roque <hadson.roque01@git config --global user.email hadson.roque01@gmail.com>
2023-03-01 17:26:25 +0100

    Create Apptrack.vue

---
+++ frontend/src/components/AppTrack.vue
@@ -0,0 +1,109 @@
+<script>
+
+export default {
+    name: 'AppTrack',
+    data(){
+        return{
+            img: "img/map-home.webp",
+            buttons: [
+                {
+                    icon: "fa-brands fa-apple",
+                    textSmaller: "Donwload on the ",
+                    textBigger: "App Store"
+                },
+                {
+                    icon: "fa-brands fa-google-play",
+                    textSmaller: "GET IT ON ",
+                    textBigger: "Google Play"
+                },
+            ]
+        }
+    }
+}
+</script>
+
+<template>
+    <section>
+        <div class="my_container">
+            <div class="txt">
+                <h1>Track orders to your door</h1> 
+                <p>
+                    Get your favourite food delivered in a flash. You’ll see when your rider’s picked up your order, and be able to follow them along the way. You’ll get a notification when they’re nearby, too.
+                </p>
+
+                <!-- buttons sx -->
+                <div class="my_buttons">
+                    <div class="my_button" v-for="(button,index) in buttons" :key="index">
+                        <div class="ms_icon">
+                            <i :class="button.icon"></i>
+                        </div>
+                        <div class="smaller">
+                            {{ button.textSmaller }}
+                            <span class="bigger">{{ button.textBigger }}</span>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+
+            <!-- parte dx -->
+            <div>
+                <img :src="img" alt="">
+            </div>
+            
+        </div>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+@use '../src/styles/general.scss' as *;
+@use '../src/styles/partials/mixins' as *;
+@use '../src/styles/partials/variables' as *;
+
+section{
+    background-color: #f9fafa;
+    padding-bottom: 100px ;
+
+    .my_container{
+        @include flex(flex);
+        border: 1px solid #f0f0f0;
+        box-shadow: 0px 20px 10px 0px #b7bec2;
+
+        .txt{
+            width: 50%;
+            padding: 30px;
+
+            p{
+                margin: 20px 0;
+            }
+        }
+
+        .my_buttons{
+            @include flex(flex);
+        }
+        .my_button{
+            width: 150px;
+            background-color: #000;
+            color: #fff;
+            line-height: 15px;
+            margin-right: 15px;
+            padding: 5px;
+            @include flex(flex);
+
+            .ms_icon{
+                width: 30px;
+                font-size: 30px;
+            }
+
+            .smaller{
+                font-size: 13px;
+                margin-left: 10px;
+            }
+
+            .bigger{
+                font-size: 18px;
+            }
+        }
+    }
+}
+</style>
