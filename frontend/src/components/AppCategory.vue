0f9f2c31b42e07e7a5f54cfa3da06ccba910866b
RominaMalimban <romina.lyn@hotmail.it>
2023-03-02 16:31:56 +0100

    modified array with objects

--- frontend/src/components/AppCategory.vue
+++ frontend/src/components/AppCategory.vue
@@ -5,18 +5,55 @@ export default {
     data(){
         return{
             categories: [
-                'img/hamburger.jpg',
-                'img/chinese.jpg',
-                'img/greek.jpg',
-                'img/indian.jpg',
-                'img/italian.jpg',
-                'img/kebab.jpg',
-                'img/mexican.jpg',
-                'img/pizza.jpg',
-                'img/sushi.jpg',
-                'img/sweet.jpg',
-                'img/vegetarian.jpg',
-                'img/breakfast.jpg'
+                {
+                    img: 'img/hamburger.jpg',
+                    typeName: 'Hamburgers'
+                },
+                {
+                    img: 'img/chinese.jpg',
+                    typeName: 'Chinese'
+                },
+                {
+                    img: 'img/greek.jpg',
+                    typeName: 'Greek'
+                },
+                {
+                    img: 'img/indian.jpg',
+                    typeName: 'Indian'
+                },
+                {
+                    img: 'img/italian.jpg',
+                    typeName: 'Italian'
+                },
+                {
+                    img: 'img/mexican.jpg',
+                    typeName: 'Mexican'
+                },
+                {
+                    img: 'img/pizza.jpg',
+                    typeName: 'Pizza'
+                },
+                {
+                    img: 'img/sushi.jpg',
+                    typeName: 'Sushi'
+                },
+                {
+                    img: 'img/sweet.jpg',
+                    typeName: 'Sweets'
+                },
+                {
+                    img: 'img/vegetarian.jpg',
+                    typeName: 'Vegan'
+                },
+                {
+                    img: 'img/kebab.jpg',
+                    typeName: 'Hamburgers'
+                },
+                {
+                    img: 'img/breakfast.jpg',
+                    typeName: 'Breakfast'
+                }
+                
             ]
         }
     }
@@ -28,8 +65,9 @@ export default {
 
         <!-- da fare carosello carino caruccio -->
         <div class="my_container">
-            <div class="category" v-for="category in categories">
-                <img :src="category" alt="">
+            <div class="category" v-for="(category, index) in categories" :key="index">
+                <img :src="category.img" :alt="category.typeName">
+                <span>{{ category.typeName }}</span>
             </div>
             
         </div>
@@ -50,6 +88,16 @@ section{
 
     .category{
         width: calc(100% / 4);
+        position: relative;
+
+        span{
+            color: #fff;
+            font-weight: bolder;
+            position: absolute;
+            bottom: 0;
+            right: 50%;
+            transform: translate(0, -50%);
+        }
     }
 }
 </style>
