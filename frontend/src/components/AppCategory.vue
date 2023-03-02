1e6353faf163dc8c82999ff85247ac415fa951b1
RominaMalimban <romina.lyn@hotmail.it>
2023-03-02 01:20:59 +0100

    insert array of images in AppCategory

--- frontend/src/components/AppCategory.vue
+++ frontend/src/components/AppCategory.vue
@@ -1,13 +1,34 @@
 <script>
 
 export default {
-    name: 'AppCategory'
+    name: 'AppCategory',
+    data(){
+        return{
+            categories: [
+                'img/chinese.jpg',
+                'img/greek.jpg',
+                'img/hambugers.jpg',
+                'img/indian.jpg',
+                'img/italian.jpg',
+                'img/kebab.jpg',
+                'img/mexican.jpg',
+                'img/pizza.jpg',
+                'img/sushi.jpg',
+                'img/sweet.jpg',
+                'img/vegetarian.jpg',
+                'img/breakfast.jpg',
+            ]
+        }
+    }
 }
 </script>
 
 <template>
     <section>
         <div class="my_container">
+            <div class="category" v-for="category in categories">
+                <img :src="category" alt="">
+            </div>
             
         </div>
     </section>
@@ -18,5 +39,7 @@ export default {
 @use '../src/styles/partials/mixins' as *;
 @use '../src/styles/partials/variables' as *;
 
-
+.category{
+    @include flex(flex);
+}
 </style>
