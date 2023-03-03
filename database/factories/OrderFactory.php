2345ee2b9b168aed425066312d10148805dd854a
Vale <oirelav95@gmail.com>
2023-03-03 16:22:15 +0100

    create OrderFactory

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -17,7 +17,14 @@ class OrderFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'create_time' => fake() -> time(),
+            'order_number' => fake() -> unique() -> randomNumber(rand(1, 6), true),
+            'total' => fake() -> randomNumber(6, false),  
+            'customer_name' => fake() -> name(),
+            'customer_surname' => fake() -> surname(),
+            'address' => fake() -> streetAddress(),
+            'email' => fake() -> email(), 
+            'phone_number' => fake() -> randomNumber(10, true),
         ];
     }
 }
