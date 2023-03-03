522e7aaae44b59ed9e777c1e53e0a5c57506b898
Vale <oirelav95@gmail.com>
2023-03-03 18:03:41 +0100

    edit migration user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('business_name');
+            $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('address');
-            $table->string('vat_number', 11) -> unique();
             $table->rememberToken();
             $table->timestamps();
         });
