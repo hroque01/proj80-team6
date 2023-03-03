91bcf0b63e61725a56c5d6712cff1b0477625c88
Vale <oirelav95@gmail.com>
2023-03-03 17:10:16 +0100

    edit DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,8 @@ class DatabaseSeeder extends Seeder
     {
         $this -> call([
 
+            UserSeeder :: class,
+            TypologySeeder :: class,
             RestaurantSeeder :: class,
 
         ]);
