172140ef2b569efba98626560a4cb35e8114931b
Hadson Roque <hadson.roque01@git config --global user.email hadson.roque01@gmail.com>
2023-03-04 23:54:47 +0100

    User.php changed

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,7 +42,8 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function restaurant() {
-        return $this -> hasOne(Restaurant::class);
+    public function restaurant()
+    {
+        return $this->belongsTo(Restaurant::class);
     }
 }
