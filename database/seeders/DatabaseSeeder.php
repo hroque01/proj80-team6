98491e7f6fcd8281093cc90f629c5f0a6e000daf
Vale <oirelav95@gmail.com>
2023-03-04 18:18:12 +0100

    edit DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,8 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this -> call([
-
-            RestaurantUserSeeder :: class,
+            UserSeeder::class,
             TypologySeeder :: class,
             RestaurantSeeder :: class,
 
