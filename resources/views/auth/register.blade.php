b65bfd0658dba0f663951d7de32c7739e39d1285
Vale <oirelav95@gmail.com>
2023-03-06 17:35:16 +0100

    edit register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -89,20 +89,6 @@
                             </div>
                         </div>
 
-                        <div class="mb-4 row">
-                            <label for="restaurant_description" class="col-md-4 col-form-label text-md-right">{{ __('Restaurant description') }}</label>
-
-                            <div class="col-md-6">
-                                <textarea id="restaurant_description" class="form-control @error('restaurant_description') is-invalid @enderror" name="restaurant_description" value="{{ old('restaurant_description') }}" ></textarea>
-
-                                @error('restaurant_description')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                            </div>
-                        </div>
-
                         <h2>
                             Typologies
                         </h2>
