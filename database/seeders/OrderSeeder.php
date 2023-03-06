4ab632df9b9a8e968ce0c0c9655943156d8a6c7a
Vale <oirelav95@gmail.com>
2023-03-06 12:18:39 +0100

    edit OrderSeeder

--- database/seeders/OrderSeeder.php
+++ database/seeders/OrderSeeder.php
@@ -5,6 +5,10 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use App\Models\Dish;
+use App\Models\Order;
+use App\Models\Restaurant;
+
 class OrderSeeder extends Seeder
 {
     /**
@@ -14,6 +18,21 @@ class OrderSeeder extends Seeder
      */
     public function run()
     {
-        //
+        
+        // Associazione di un ordine al primo ristorante random
+        // Associazione di un ordine da 1 a 5 piatti random
+        Order :: factory() -> count(10) -> make() -> each(function($order) {
+            
+            $restaurant = Restaurant::inRandomOrder() -> first();
+            // Relazione 1 to M 
+            $order -> restaurant() -> associate($restaurant);
+            $order -> save();
+
+            // Relazione M to M
+            $dishes = Dish::inRandomOrder() -> limit(rand(1,5)) -> get();
+
+            $order -> dishes() -> attach($dishes);
+
+        });
     }
 }
