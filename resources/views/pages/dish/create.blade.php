f9b44fbeaf8ca54c188080399a7b2336100eddd8
Vale <oirelav95@gmail.com>
2023-03-08 13:42:57 +0100

    edit create

---
+++ resources/views/pages/dish/create.blade.php
@@ -0,0 +1,69 @@
+@extends('layouts.main-layout')
+
+@section('content')
+
+    <div class="container pt-5">
+        <h1>
+            Create new dish
+        </h1>
+
+        <div class="col-md-8">
+
+            <form method="POST" action="{{ route('dish.store') }}" enctype="multipart/form-data">
+
+                @csrf
+                <div class="mb-5 row">
+                    <label for="name" class="col-md-2 col-form-label text-md-right">Name</label>
+                    <div class="col-md-5">
+                        <input type="text" name="name" class="form-control">
+                    </div>
+                </div>
+
+                <div class="mb-5 row">
+                    <label for="description" class="col-md-2 col-form-label text-md-right">Description</label>
+                    <div class="col-md-5">
+                        <input type="text" name="description" class="form-control">
+                    </div>
+                </div>
+
+                <div class="mb-5 row">
+                    <label for="ingredients" class="col-md-2 col-form-label text-md-right">Ingredients</label>
+                    <div class="col-md-5">
+                        <textarea name="ingredients" class="form-control"></textarea>
+                    </div>
+                </div>
+                
+                <div class="mb-5 row">
+                    <label for="price" class="col-md-2 col-form-label text-md-right">Price</label>
+                    <div class="col-md-5">
+                        <input type="number" step=".01" name="price" class="form-control">
+                    </div>
+                </div>
+
+                <div class="mb-5 row">
+                    <label for="image" class="col-md-2 col-form-label text-md-right">Photo</label>
+                    <div class="col-md-5">
+                        <input type="file" name="image" class="form-control">
+                    </div>
+                </div>
+
+                <div class="col-md-3">
+                    <select name="restaurant_id">
+                        @foreach ($restaurants as $restaurant)
+                            <option value="{{ $restaurant -> id }}">{{ $restaurant->user -> business_name }}</option>    
+                        @endforeach
+                    </select>
+                </div>
+
+                
+                <div class="mb-5 row mb-0">
+                    <div class="col-md-5 offset-md-4">
+                        <input type="submit" value="Create" class="btn btn-primary">
+                    </div>
+                </div>
+            </form>
+
+        </div>
+    </div>
+
+@endsection
