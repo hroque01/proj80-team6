0f6e650c8cba8cfdc44e2f0d5990fd6294a5085d
Vale <oirelav95@gmail.com>
2023-03-08 10:46:34 +0100

    edit home

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -11,5 +11,24 @@
             <a href="{{ route('dishCreate') }}">Create new dish</a>
         </h2>
 
+        <h3>
+            Your dishes: 
+        </h3>
+        <div class="row">
+            @foreach($dishes as $dish)
+              <div class="col-md-4">
+                <div class="card">
+                  <img class="card-img-top" src="{{ asset($dish->image) }}" alt="{{ $dish->name }}">
+                  <div class="card-body">
+                    <h5 class="card-title">{{ $dish->name }}</h5>
+                    <p class="card-text">{{ $dish->description }}</p>
+                    <p class="card-text">{{ $dish->price }} €</p>
+                  </div>
+                </div>
+              </div>
+            @endforeach
+          </div>
+    </div>
+
     </div>
 @endsection
