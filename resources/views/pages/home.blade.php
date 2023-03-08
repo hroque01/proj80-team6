fe1bf9d0192eb0f02296e68ad95364b1a3160d37
RominaMalimban <romina.lyn@hotmail.it>
2023-03-08 12:23:04 +0100

    fixed problem with img

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,33 +2,38 @@
 
 @section('content')
 
-    <div class="container pt-5">
-        <h1>
-            Your restaurant: {{Auth::user()->business_name}}
-        </h1>
+  <div class="container pt-5">
+    <h1>
+      Your restaurant: {{Auth::user()->business_name}}
+    </h1>
 
-        <h2>
-            <a href="{{ route('dishCreate') }}">Create new dish</a>
-        </h2>
+    <h2>
+        <a href="{{ route('dishCreate') }}">Create new dish</a>
+    </h2>
 
-        <h3>
-            Your dishes: 
-        </h3>
-        <div class="row">
-            @foreach($dishes as $dish)
-              <div class="col-md-4">
-                <div class="card">
-                  <img class="card-img-top" src="{{ asset($dish->image) }}" alt="{{ $dish->name }}">
-                  <div class="card-body">
-                    <h5 class="card-title">{{ $dish->name }}</h5>
-                    <p class="card-text">{{ $dish->description }}</p>
-                    <p class="card-text">{{ $dish->price }} €</p>
-                  </div>
-                </div>
-              </div>
-            @endforeach
+    <h3>
+        Your dishes: 
+    </h3>
+    <div class="row">
+      @foreach($dishes as $dish)
+        <div class="col-md-4">
+
+          <div class="card">
+
+            {{-- img caricamento: --}}
+            <img class="card-img-top" src="{{ asset($dish->image) }}" alt="{{ $dish->name }}">
+            <img src="{{ asset('storage/' . $dish -> image) }}" alt="">
+
+            <div class="card-body">
+              <h5 class="card-title">{{ $dish->name }}</h5>
+              <p class="card-text">{{ $dish->description }}</p>
+              <p class="card-text">{{ $dish->price }} €</p>
+            </div>
           </div>
-    </div>
 
+        </div>
+      @endforeach
     </div>
+  </div>
+
 @endsection
