4c4f77f27e1e190db9d9e28844b34fbcd143e4b0
Vale <oirelav95@gmail.com>
2023-03-10 16:43:59 +0100

    create store.js file for buttons in main and footer

---
+++ frontend/src/store.js
@@ -0,0 +1,16 @@
+import { reactive } from 'vue'
+
+export const store = reactive({
+    buttons: [
+        {
+            icon: "fa-brands fa-apple",
+            textSmaller: "Donwload on the ",
+            textBigger: "App Store"
+        },
+        {
+            icon: "fa-brands fa-google-play",
+            textSmaller: "GET IT ON ",
+            textBigger: "Google Play"
+        },
+    ]
+});
