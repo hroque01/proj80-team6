e15ca0d6642ef6d5f33dbea369122d7ad3766d75
Vale <oirelav95@gmail.com>
2023-03-10 17:55:54 +0100

    edit file dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,12 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container pt-5">
-        @guest
-            <h3>
-                Registrazione avvenuta con successo!
-            </h3>
-        @endguest
-    </div>
+<div class="container">
+    <div class="row justify-content-center pt-5">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
 
+                <div class="card-body">
+                    @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
+                    @endif
+
+                    {{ __('Registrazione avvenuta con successo!') }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
