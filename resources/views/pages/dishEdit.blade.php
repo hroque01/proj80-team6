3b01b26eed60dda5bdc79965e41bb298b1d2abdc
Vale <oirelav95@gmail.com>
2023-03-10 19:28:38 +0100

    add required and visible in file dishEdit

--- resources/views/pages/dishEdit.blade.php
+++ resources/views/pages/dishEdit.blade.php
@@ -13,64 +13,59 @@
             <div class="mb-5 row">
                 <label for="name" class="col-md-2 col-form-label text-md-right">Nome</label>
                 <div class="col-md-5">
-                    <input type="text" name="name" class="form-control" value="{{$dish -> name}}">
+                    <input type="text" name="name" class="form-control" value="{{$dish -> name}}" required>
                 </div>
             </div>
 
             <div class="mb-5 row">
                 <label for="description" class="col-md-2 col-form-label text-md-right">Descrizione</label>
                 <div class="col-md-5">
-                    <input type="text" name="description" class="form-control" value="{{$dish -> description}}">
+                    <input type="text" name="description" class="form-control" value="{{$dish -> description}}" required>
                 </div>
             </div>
 
             <div class="mb-5 row">
                 <label for="ingredients" class="col-md-2 col-form-label text-md-right">Ingredients</label>
                 <div class="col-md-5">
-                    <textarea name="ingredients" class="form-control" value="{{$dish -> ingredients}}"></textarea>
+                    <input type="text" name="ingredients" class="form-control" value="{{$dish -> ingredients}}" required>
                 </div>
             </div>
             
             <div class="mb-5 row">
                 <label for="price" class="col-md-2 col-form-label text-md-right">Price</label>
                 <div class="col-md-5">
-                    <input type="number" step=".01" name="price" class="form-control" value="{{$dish -> price}}">
+                    <input type="number" step=".01" name="price" class="form-control" value="{{$dish -> price}}" required>
                 </div>
             </div>
 
             <div class="mb-5 row">
                 <label for="image" class="col-md-2 col-form-label text-md-right">Photo</label>
                 <div class="col-md-5">
-                    <input type="file" name="image" class="form-control" value="{{$dish -> image}}">
+                    <input type="file" name="image" class="form-control" value="{{$dish -> image}}" required>
                 </div>
             </div>
 
-            {{-- <div class="col-md-3">
-                <select name="restaurant_id">
-                    @foreach ($restaurants as $restaurant)
-                        <option value="{{ $restaurant -> id }}"
-                            @if ($dish -> restaurant -> id === $restaurant -> id)
-                                selected
-                            @endif
-                            >{{ $restaurant->user -> business_name }}</option>    
-                    @endforeach
-                </select>
-            </div> --}}
-
-            {{-- <div class="mb-5 row">
+            <div class="mb-5 row">
                 <div class="col-md-5">
                     <h2>
                         Visible
                     </h2>
-                    <input type="radio" id="scelta1" name="visible" value="1" checked>
+                    <input type="radio" id="scelta1" name="visible" value="1" 
+                    @if ($dish->visible)
+                        checked
+                    @endif
+                    >
                     <label for="1">Si</label>
 
-                    <input type="radio" id="scelta2" name="visible" value="0">
+                    <input type="radio" id="scelta2" name="visible" value="0"
+                    @if (!$dish->visible)
+                        checked
+                    @endif
+                    >
                     <label for="0">No</label>
                 </div>
-            </div> --}}
+            </div>
 
-            
             <div class="mb-5 row mb-0">
                 <div class="col-md-5 offset-md-4">
                     <input type="submit" value="UPDATE" class="btn btn-primary">
