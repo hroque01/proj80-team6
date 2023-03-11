096d916fee6f4b5cd8e12a946aa1160068b22b72
Vale <oirelav95@gmail.com>
2023-03-11 16:55:00 +0100

    add router for getDishes in api

--- routes/api.php
+++ routes/api.php
@@ -7,3 +7,4 @@ use App\Http\Controllers\ApiController;
 
 Route::get('/v1/typology/all', [ApiController::class, 'getAllTypologies'])->name('typology.all');
 Route::get('/v1/restaurant/filtered', [ApiController::class, 'getFilteredRestaurants'])->name('restaurant.filtered');
+Route::get('/v1/dish/all', [ApiController::class, 'getDishes'])->name('dish.all');
