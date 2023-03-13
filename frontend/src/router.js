b80cc0ed470a19f556c4116d1fa2d0cdf0982fbe
Vale <oirelav95@gmail.com>
2023-03-13 16:10:00 +0100

    add routes in route.js

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,13 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+import AppRestaurantMainPage from './components/AppRestaurantMainPage.vue';
+
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        // {
-        //     path: '/dishes/:id',
-        //     name: 'dish-detail',
-        //     component: AppDishes
-        // },
+        {
+            path: '/restaurant/dishes/:id',
+            name: 'restaurant-detail',
+            component: AppRestaurantMainPage
+        },
     ]
 });
 export default router;
