36949ed7ffdace564a371f932c7cb73b5e7cc47c
Vale <oirelav95@gmail.com>
2023-03-13 16:10:53 +0100

    edit route in api.php

--- routes/api.php
+++ routes/api.php
@@ -7,4 +7,4 @@ use App\Http\Controllers\ApiController;
 
 Route::get('/v1/typology/all', [ApiController::class, 'getAllTypologies'])->name('typology.all');
 Route::get('/v1/restaurant/filtered', [ApiController::class, 'getFilteredRestaurants'])->name('restaurant.filtered');
-Route::get('/v1/dish/all', [ApiController::class, 'getDishes'])->name('dish.all');
+Route::get('/v1/restaurant/{restaurant}', [ApiController::class, 'getDishesAndRestaurants'])->name('dish.all');
