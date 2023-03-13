a14c22c08e0d3801a02d5ddb4e2a880eead0afe1
Vale <oirelav95@gmail.com>
2023-03-13 19:38:01 +0100

    add route for home in router.js

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -4,9 +4,16 @@ import AppRestaurantMainPage from './components/AppRestaurantMainPage.vue';
 
 import AppCheckout from './components/AppCheckout.vue';
 
+import App from './App.vue';
+
 const router = createRouter({
     history: createWebHistory(),
     routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: App
+        },
         {
             path: '/restaurant/dishes/:id',
             name: 'restaurant-detail',
