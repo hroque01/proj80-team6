545ef6fe853bddb95779373b2f34a71f73bbdf96
Vale <oirelav95@gmail.com>
2023-03-13 16:10:34 +0100

    edit AppRestaurant

--- frontend/src/components/AppRestaurant.vue
+++ frontend/src/components/AppRestaurant.vue
@@ -1,13 +1,20 @@
 <script>
 
+import AppRestaurantMainPage from './AppRestaurantMainPage.vue';
+
 import axios from 'axios';
+import { store } from '../store';
 
 const API_URL = 'http://localhost:8000/api/v1/';
 
 export default {
     name: 'AppRestaurant',
+    components: {
+        AppRestaurantMainPage,
+    },
     data() {
         return {
+            store,
             typologies: [],
             restaurants: [],
             // proprietà con array che conterrà le tipologie selezionate dall'utente:
@@ -34,6 +41,7 @@ export default {
                     if (success) {
                         this.typologies = typologies;
                         this.restaurants = restaurants;
+
                     }
                 })
                 .catch(err => console.error(err));
@@ -128,8 +136,8 @@ export default {
                                     </div>
                                 </div>
 
-                                <!-- <router-link :to="{ name: 'dish-detail', params: { id: restaurant.id } }">Vai qui</router-link> -->
-
+                                <router-link :to="{ name: 'restaurant-detail', params: { id: restaurant.id } }">Vai
+                                    qui</router-link>
 
                             </div>
 
@@ -144,6 +152,8 @@ export default {
         </div>
         <!-- chiusura div container -->
     </section>
+
+    <router-view></router-view>
 </template>
 
 <style lang="scss" scoped>
