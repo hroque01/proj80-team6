e7604928b79653bf4342a106ec7239477c196523
Vale <oirelav95@gmail.com>
2023-03-15 18:16:32 +0100

    create AppPayment

---
+++ frontend/src/components/AppPayment.vue
@@ -0,0 +1,16 @@
+<script>
+
+export default {
+    name: 'AppPayment',
+}
+</script>
+
+<template>
+    Info Pagamento
+</template>
+
+<style lang="scss">
+@use '../src/styles/general.scss' as *;
+@use '../src/styles/partials/mixins' as *;
+@use '../src/styles/partials/variables' as *;
+</style>
