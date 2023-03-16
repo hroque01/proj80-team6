975b9945d223ecc1f8285aba479d745037265343
RominaMalimban <romina.lyn@hotmail.it>
2023-03-16 23:01:29 +0100

    added function for restaurant_id

--- frontend/src/components/AppData.vue
+++ frontend/src/components/AppData.vue
@@ -14,8 +14,10 @@ export default {
                 address: '',
                 email: '',
                 phone_number: '',
-                total: 0
-            }
+                total: 0,
+                restaurantId: ''
+            },
+            restaurantId: null,
         };
     },
     methods: {
@@ -49,9 +51,24 @@ export default {
                 })
                 .catch(err => console.log(err));
         },
+        findRestaurant(){
+            const cartItem = localStorage.getItem('storedQuantity_0');
+            let element;
+
+            if(cartItem){
+                element= JSON.parse(cartItem);
+
+                this.restaurantId = element.restaurant_id;
+
+                this.newOrder.restaurantId = this.restaurantId;
+
+                console.log(this.newOrder.restaurantId)
+            }
+        }
     },
     mounted() {
         this.updateOrders();
+        this.findRestaurant();
     }
 };
 </script>
@@ -73,25 +90,25 @@ export default {
         <div class="flex-form">
             <label for="customer_name">Inserisci il tuo nome e cognome<span>*</span></label>
             <input type="text" placeholder="Mario Rossi" name="customer_name" id="customer_name"
-                v-model="newOrder.customer_name">
+                v-model="newOrder.customer_name" required>
         </div>
 
         <!-- indirizzo -->
         <div class="flex-form">
             <label for="address">Inserisci il tuo indirizzo<span>*</span></label>
-            <input type="text" placeholder="Via Roma, 10" name="address" v-model="newOrder.address">
+            <input type="text" placeholder="Via Roma, 10" name="address" v-model="newOrder.address" required>
         </div>
 
         <!-- email -->
         <div class="flex-form">
             <label for="email">Inserisci la tua email<span>*</span></label>
-            <input type="email" placeholder="email@prova.it" name="email" v-model="newOrder.email">
+            <input type="email" placeholder="email@prova.it" name="email" v-model="newOrder.email" required>
         </div>
 
         <!-- telefono -->
         <div class="flex-form">
             <label for="phone_number">Inserisci il tuo telefono<span>*</span></label>
-            <input type="text" placeholder="3468888888" name="phone_number" v-model="newOrder.phone_number">
+            <input type="text" placeholder="3468888888" name="phone_number" v-model="newOrder.phone_number" required>
         </div>
 
 
