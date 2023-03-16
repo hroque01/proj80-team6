43ce3ce1bd679f2bd36cc6c9726be49ed87b0a9c
RominaMalimban <romina.lyn@hotmail.it>
2023-03-16 12:29:11 +0100

    added form in AppData

--- frontend/src/components/AppData.vue
+++ frontend/src/components/AppData.vue
@@ -8,24 +8,25 @@ export default {
 <template>
     Info Dati
 
-    <form action="">
-        <div>
-            <label for="">Inserisci il tuo nome e cognome*</label>
-            <input type="text" placeholder="Mario Rossi">
+    <form>
+   
+        <div class="flex-form">
+            <label for="customer_name">Inserisci il tuo nome e cognome<span>*</span></label>
+            <input type="text" placeholder="Mario Rossi" name="customer_name" required id="customer_name">
         </div>
-        <div>
-            <label for="">Inserisci il tuo indirizzo*</label>
-            <input type="text" placeholder="Via Roma, 10">
+        <div class="flex-form">
+            <label for="address">Inserisci il tuo indirizzo<span>*</span></label>
+            <input type="text" placeholder="Via Roma, 10" name="address" required id="address">
         </div>
-        <div>
-            <label for="">Inserisci il tuo nome</label>
-            <input type="text">
+        <div class="flex-form">
+            <label for="email">Inserisci la tua email<span>*</span></label>
+            <input type="email" placeholder="email@prova.it" name="email" required id="email">
         </div>
-        <div>
-            <label for="">Inserisci il tuo nome</label>
-            <input type="text">
+        <div class="flex-form">
+            <label for="phone_number">Inserisci il tuo telefono<span>*</span></label>
+            <input type="text" placeholder="3468888888" name="phone_number" required id="phone_number">
         </div>
-        
+        <input type="submit" value="Invia">
     </form>
 </template>
 
@@ -33,4 +34,35 @@ export default {
 @use '../src/styles/general.scss' as *;
 @use '../src/styles/partials/mixins' as *;
 @use '../src/styles/partials/variables' as *;
+
+form{
+    width: 600px;
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    // debug
+    border: 1px solid black;
+
+    .flex-form{
+        @include flex(between);
+        margin: 10px 0;
+        
+        span{
+            color: $btn_red;
+        }
+
+        input{
+            padding: 5px;
+            width: 55%;
+        }
+    }
+
+    input[type="submit"]{
+        align-self: center;
+        margin-top: 10px;
+        padding: 0 15px;
+    }
+
+}
+
 </style>
