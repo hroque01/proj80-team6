24e660137bafacb38e6d8e3fbb27295037cd12aa
Vale <oirelav95@gmail.com>
2023-03-16 12:35:24 +0100

    add route in api.php

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\ApiController;
 
-Route::get('/v1/typology/all', [ApiController::class, 'getAllTypologies'])->name('typology.all');
-Route::get('/v1/restaurant/filtered', [ApiController::class, 'getFilteredRestaurants'])->name('restaurant.filtered');
-Route::get('/v1/restaurant/{restaurant}', [ApiController::class, 'getDishesAndRestaurants'])->name('dish.all');
+Route::get('/v1/typology/all', [ApiController::class, 'getAllTypologies']);
+Route::get('/v1/restaurant/filtered', [ApiController::class, 'getFilteredRestaurants']);
+Route::get('/v1/restaurant/{restaurant}', [ApiController::class, 'getDishesAndRestaurants']);
+
+Route::post('/v1/order/store', [ApiController::class, 'orderStore']);
