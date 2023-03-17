33af30add1fc31fd9ec4434ae9c8cf512cfa14a3
RominaMalimban <romina.lyn@hotmail.it>
2023-03-17 20:37:06 +0100

    orders and dishes show

---
+++ resources/views/pages/orderShow.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.header')
+
+@section('content')
+
+  <div class="my_container pt-5">
+    
+    <div class="card p-5">
+        <h1>Ordine #{{$order-> order_number}}</h1>
+
+        <div class="orders">
+            <div class="sx-order"> Data e ora ordine:</div>
+            <div>{{$order-> create_date}} - {{$order-> create_time}}</div>
+        </div>
+        <div class="orders">
+            <div class="sx-order">Nome cliente:</div>
+            <div>{{$order-> customer_name}}</div>
+        </div>
+        <div class="orders">
+            <div class="sx-order">Indirizzo cliente:</div>
+            <div>{{$order-> address}}</div>
+        </div>
+        <div class="orders">
+            <div class="sx-order">Indirizzo email cliente:</div>
+            <div>{{$order-> email}}</div>
+        </div>
+        <div class="orders">
+            <div class="sx-order">Telefono cliente:</div>
+            <div>{{$order-> phone_number}}</div>
+        </div>
+        <div class="orders">
+            <div class="sx-order">Totale ordine:</div>
+            <div class="total-order bg-danger text-light">{{$order-> total}}&euro;</div>
+        </div>
+
+        <a class="mt-5" href="{{route('order.home')}}">Torna alla lista degli ordini</a>
+      </div>
+
+      
+    </div>
+   
+
+@endsection
