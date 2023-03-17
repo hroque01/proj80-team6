264136791e911b7450aeb10897c648f1fdf6d248
Hadson Roque <nineonezero.light@gmail.com>
2023-03-17 12:11:43 +0100

    Best commit ever 200IQ

--- database/seeders/OrderSeeder.php
+++ database/seeders/OrderSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use Database\Factories\OrderFactory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+
 
 use App\Models\Dish;
 use App\Models\Order;
@@ -18,27 +20,6 @@ class OrderSeeder extends Seeder
      */
     public function run()
     {
-         $orders = [
-            //  [
-            //      'create_time' => '19:00',
-            //      'order_number' => '324562',
-            //      'total' => 15.00,
-            //      'customer_name' => 'Mario Rossi',
-            //      'address' => 'Via Mario Rossi, 69',
-            //      'email' => 'mariorossi@gmail.com',
-            //      'phone_number' => '3334445557',
-            //      'completed' => false,
-            //      'restaurant_id'=> 2,
-            //  ],
-            
-         ];
-
-         foreach ($orders as $order) {
-             $order = Order::create($order);
-         }
-
-        //  $order_1 = Order::find(1);
-        //  $dish_1 = Dish::find([2]);
-        //  $order_1->dishes()->attach($dish_1);
+        Order::factory()->count(1000)->create();
     }
 }
