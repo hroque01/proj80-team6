5438c9b19193f978a6ab9b05cfe7b9b116332fbb
RominaMalimban <romina.lyn@hotmail.it>
2023-03-18 21:22:01 +0100

    removed cdn fontawesome

--- resources/views/layouts/appRegister.blade.php
+++ resources/views/layouts/appRegister.blade.php
@@ -14,7 +14,6 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
     <!-- Usando Vite -->
     @vite(['resources/js/app.js'])
 </head>
