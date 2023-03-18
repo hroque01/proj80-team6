329fc60dd837c09926b6045b822fc984096548c7
RominaMalimban <romina.lyn@hotmail.it>
2023-03-18 18:41:15 +0100

    modified route

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -29,7 +29,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended('restaurant');
+        return redirect()->intended('dashboard');
     }
 
     /**
